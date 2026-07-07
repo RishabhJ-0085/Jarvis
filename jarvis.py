e01e0e1f4133ac5baf928d72e9b6b41efc0118c9
RishabhJ-0085 <rishabhjaiswal2731@>
2026-07-07 15:12:16 +0530

    ADD Joke

--- jarvis.py
+++ jarvis.py
@@ -237,7 +237,7 @@ def takeCommand():
         elif "joke" in query:
             j = jk.get_joke()
             print(j)
-            speck(j)
+            speck("her is joke "+j)
         elif "bad" in query:
             speck("Sorry sir but i try my best")
         elif "good" in query or "nice" in query:

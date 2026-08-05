e5c71fb52330aac86fc9e7a8a73656838dd2d0bf
RishabhJ-0085 <rishabhjaiswal2731@>
2026-08-05 15:41:42 +0530

    testing

--- install_requirements.py
+++ install_requirements.py
@@ -1,25 +1,45 @@
-import os
 import subprocess
 import sys
+import os
 
 FLAG_FILE = ".requirements_installed"
 
 # Agar pehle install ho chuki hain to dobara install mat karo
-if not os.path.exists(FLAG_FILE):
-
+if os.path.exists(FLAG_FILE):
+    with open("Install_requirements.txt",'r') as New_lib:
+        New_lib = New_lib.read().splitlines()
+        with open("requirements.txt",'r') as old_lib:
+            Old_lib = old_lib.read().splitlines()
+            New_set = set(New_lib)         
+            Old_set = set(Old_lib)
+            missing = New_set - Old_set
+            for i in missing:
+                print("Installing required libraries...")
+                subprocess.check_call([
+                    sys.executable,
+                    "-m",
+                    "pip",
+                    "install",
+                    i
+                ])
+                with open("requirements.txt",'a') as old_lib:
+                    old_lib.write(i+'\n')
+    print("All libraries installed.")
+else:
     print("Installing required libraries...")
-
     subprocess.check_call([
         sys.executable,
         "-m",
         "pip",
         "install",
         "-r",
-        "requirements.txt"
+        "install_requirements.txt"
     ])
-
-    # Flag file bana do
     with open(FLAG_FILE, "w") as f:
         f.write("Installed")
-
+    with open("Install_requirements.txt",'r') as New_lib:
+        New_lib = New_lib.read().splitlines()
+        with open('requirements.txt', 'w') as fill:
+            for i in New_lib:
+                fill.write(i+'\n')
     print("All libraries installed.")
